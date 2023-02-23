b57c551c15aa1fd00ce9ee995df8c429d8bcf981
Brian Pérez López <brianpl990227@gmail.com>
2023-02-23 00:02:50 -0500

    deploy base

--- src/Helpers/UrlRequest.js
+++
@@ -1,9 +0,0 @@
-const APIDomain = "http://localhost:5214/api/"
-export const APIFiles = "http://localhost:5138/"
-
-//Users
-export const RegisterUrl = APIDomain + "user/register"
-export const LoginUrl = APIDomain + "user/login"
-export const UpdateTimeUrl = APIDomain + "user/timer"
-export const GetTodayTimer = APIDomain + "User/today"
-export const GetAllUsers = APIDomain + "user"
