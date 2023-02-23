f36671c195a9e7faf4f1f3fb2e67dbc87f7e7ca6
Brian Pérez López <brianpl990227@gmail.com>
2023-02-23 12:49:54 -0500

    years

--- src/components/Work.vue
+++ src/components/Work.vue
@@ -5,6 +5,28 @@
     <div class="background-page">
         <v-container style="margin-top: -100px; position: relative;">
             <v-timeline align="start">
+            
+            <v-timeline-item size="small" dot-color="teal">
+                    <template v-slot:opposite>
+                        <div :class="`pt-1 headline font-weight-bold text-red`" v-text="'December 2022 - February 2023'">
+                        </div>
+                    </template>
+                    <div>
+                        <h3 class="text-white">Salas Transportation Inc - New York, United States Of America</h3>
+                        <h4 class="text-white">Fullstack developer in Payroll Manager System:</h4>
+                        <ul>
+                            <li class="text-white">Visual interface design</li>
+                            <li class="text-white">Implementation of the web client using Vue.js</li>
+                            <li class="text-white">Implementing web client security using Json Web Token</li>
+                            <li class="text-white">Consuming services offered by the project backend</li>
+                            <li class="text-white">Server-side implementation</li>
+                            <li class="text-white">Creation of engineering artifacts for the implementation of business logic</li>
+                            <li class="text-white">Database design and use of EntityFramework code first</li>
+                            <li class="text-white">Using Json web Token as a security scheme on the server side</li>
+                        </ul>
+                        <p class="text-white margin-top-10">Reference contact: guillesp67@gmail.com</p>
+                    </div>
+                </v-timeline-item>
                 <v-timeline-item size="small" dot-color="teal">
                     <template v-slot:opposite>
                         <div :class="`pt-1 headline font-weight-bold text-red`" v-text="'December 2021 - October 2022'">
@@ -23,27 +45,7 @@
                         <p class="text-white margin-top-10">Reference contact: aorellana@uci.cu</p>
                     </div>
                 </v-timeline-item>
-                <v-timeline-item size="small" dot-color="teal">
-                    <template v-slot:opposite>
-                        <div :class="`pt-1 headline font-weight-bold text-red`" v-text="'December 2021 - October 2022'">
-                        </div>
-                    </template>
-                    <div>
-                        <h3 class="text-white">Salas Transportation Inc - New York, United States Of America</h3>
-                        <h4 class="text-white">Fullstack developer in Payroll Manager System, December 2022 - February 2023:</h4>
-                        <ul>
-                            <li class="text-white">Visual interface design</li>
-                            <li class="text-white">Implementation of the web client using Vue.js</li>
-                            <li class="text-white">Implementing web client security using Json Web Token</li>
-                            <li class="text-white">Consuming services offered by the project backend</li>
-                            <li class="text-white">Server-side implementation</li>
-                            <li class="text-white">Creation of engineering artifacts for the implementation of business logic</li>
-                            <li class="text-white">Database design and use of EntityFramework code first</li>
-                            <li class="text-white">Using Json web Token as a security scheme on the server side</li>
-                        </ul>
-                        <p class="text-white margin-top-10">Reference contact: guillesp67@gmail.com</p>
-                    </div>
-                </v-timeline-item>
+                
             </v-timeline>
 
         </v-container>
