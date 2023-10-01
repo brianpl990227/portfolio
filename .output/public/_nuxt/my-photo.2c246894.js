e9dd443d128af1c77948d2b65fdfea9cae479145
Brian Perez <brianpl990227@gmail.com>
2023-10-01 06:59:48 -0400

    descargar cv ok

--- .output/public/_nuxt/my-photo.2c246894.js
+++
@@ -1,1 +0,0 @@
-import"./entry.f140e320.js";const s=""+globalThis.__publicAssetsURL("img/my-photo.jpeg");export{s as _};
---
+++ .output/public/_nuxt/my-photo.56684a24.js
@@ -0,0 +1,1 @@
+import"./entry.6dc9abd0.js";const s=""+globalThis.__publicAssetsURL("img/my-photo.jpeg");export{s as _};
